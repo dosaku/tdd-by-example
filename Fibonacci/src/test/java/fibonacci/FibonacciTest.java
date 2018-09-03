7fc6296bd70f61175212400b3987388b35a90f48
Ryan <rjminniear@gmail.com>
2018-09-03 14:53:05 -0700

    Clean up test code.

--- Fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ Fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -1,17 +1,20 @@
 package fibonacci;
 
-import static org.junit.Assert.assertEquals;
 import static fibonacci.Fibonacci.fib;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class FibonacciTest {
 
-	@Test
-	public void testFibonacci() {
-		int[][] cases = { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 } };
-		for (int i = 0; i < cases.length; i++)
-			assertEquals(cases[i][1], fib(cases[i][0]));
-	}
+    @Test
+    public void testFibonacci() {
+        int[][] cases = { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 } };
+        for (int[] currentCase : cases) {
+            int n = currentCase[0];
+            int expected = currentCase[1];
+            assertEquals(expected, fib(n));
+        }
+    }
 
 }
